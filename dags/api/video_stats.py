cd9b0b5dd83e1bcdf8bcb325fd7765c72a06d193
Hariharan <hariharanbalaji26@gmail.com>
2026-03-19 23:06:45 +0530

    docker airflow changes

--- dags/api/video_stats.py
+++ dags/api/video_stats.py
@@ -1,14 +1,18 @@
 import requests
 import json
 from datetime import date
-import os
-from dotenv import load_dotenv
-load_dotenv(dotenv_path="./.env")
+from airflow.decorators import task
+from airflow.models import Variable
+# import os
+# from dotenv import load_dotenv
+# load_dotenv(dotenv_path="./.env")
 
-API_KEY = os.getenv('API_KEY')
-CHANNEL_NAME = "MrBeast"
+
+API_KEY = Variable.get('API_KEY')
+CHANNEL_NAME = Variable.get("CHANNEL_HANDLE")
 maxResults = 50
 
+@task
 def get_playlist_id():
     try:
         url = f"https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle={CHANNEL_NAME}&key={API_KEY}"
@@ -23,6 +27,7 @@ def get_playlist_id():
     except requests.exceptions.RequestException as e:
         raise e
     
+@task
 def get_video_ids(playlistId):
 
     video_ids = []
@@ -49,6 +54,7 @@ def get_video_ids(playlistId):
     except requests.exceptions.RequestException as e:
         raise e
 
+@task
 def extract_video_data(video_ids):
     extracted_data = []
     
@@ -86,6 +92,7 @@ def extract_video_data(video_ids):
     except requests.exceptions.RequestException as e:
         raise e
     
+@task
 def save_to_json(extracted_data):
     file_path = f'./data/YT_data_{date.today()}.json'
     
